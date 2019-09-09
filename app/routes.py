7b5a8c27ee2db15020858b168ebc0a02e3f0523a
derek-shing <hpshing@gmail.com>
2019-09-09 13:27:02 -0700

    render map based on the address user input

--- app/routes.py
+++ app/routes.py
@@ -9,28 +9,30 @@ from app.addressform import AddressForm
 from flask import request
 from app.meter_map import Meter_Map
 
-path =os.getcwd()
+#path =os.getcwd()
 
 
 
 
-x = 34.057219
-y = -118.268751
-m = folium.Map(location=[x, y])
-final_df = pd.read_csv(path+'/Parking_Meter_Inventory.csv')
-final_df = final_df.dropna(axis=0, subset=['LatLng'])
+#x = 34.057219
+#y = -118.268751
+#m = folium.Map(location=[x, y])
+#final_df = pd.read_csv(path+'/Parking_Meter_Inventory.csv')
+3final_df = final_df.dropna(axis=0, subset=['LatLng'])
 
-final_df['LatLng'] = final_df['LatLng'].apply(make_tuple)
-test_df = final_df.head(20)
+#final_df['LatLng'] = final_df['LatLng'].apply(make_tuple)
+#test_df = final_df.head(20)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    global m
-    for point in test_df['LatLng']:
-        folium.Marker(point).add_to(m)
+    #global m
+    #for point in test_df['LatLng']:
+    #    folium.Marker(point).add_to(m)
     #return render_template('index.html', title="LA Parking Space", content=m)
-    return m.get_root().render()
+    #return m.get_root().render()
+    m = Meter_Map()
+    return m.render().get_root().render()
 
 
 @app.route('/address',methods=['GET', 'POST'])
