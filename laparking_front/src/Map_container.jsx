d01612cfc492baf620e4bee5f780b521ed926fe7
derek-shing <46395519+derek-shing@users.noreply.github.com>
2020-04-21 02:45:58 -0700

    Add files via upload

---
+++ laparking_front/src/Map_container.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+function Map_container(){
+  const position = [34.057219, -118.268751]
+const map = (
+  <Map center={position} zoom={13}>
+    <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+    />
+    <Marker position={position}>
+      <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+    </Marker>
+  </Map>
+)
+
+  return(
+    <div className='map_container'>
+      <h1>Los Angeles Parking Meter's Occupancy State</h1>
+      {map}
+
+    </div>
+  )
+}
+
+export default Map_container;
